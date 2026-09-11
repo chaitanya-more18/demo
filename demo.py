be1f87eeb06026372a55573bd2325afe654a75cd
chaitanya <chaitanyamore002@gmail.com>
2026-09-11 11:26:24 +0530

    version 2.0.1

--- demo.py
+++ demo.py
@@ -1,6 +1,75 @@
-print("Hello")
+# is_Raining = False
+# if is_Raining:
+#     print("Raining Outside")
+# else:
+#     print("Not Raining")
 
-# chaitanya
-# comment 2
-# comment 3
-# comment 4
+# age = int(input("Enter age : "))
+# if age>=18:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# day = int(input("Enter day no: "))
+# match day:
+#     case 1 :
+#         print("Monday")
+#     case 2 :
+#         print("Tuesday")
+#     case 3 :
+#         print("Wednesday")
+#     case _:
+#         print("Invalid")
+
+# no = int(input("Enter a no. : "))
+# if no%2 == 0:
+#     print(f"{no} is even")
+# else:
+#     print(f"{no} is odd")
+
+# age = int(input("Enter age : "))
+# price = 500
+# if age<12:
+#     print("10 percent discount, Amount is", price-(price*10/100))
+# else:
+#     print("No discount, Amount is", price)
+
+# marks = int(input("Enter you marks : "))
+# if marks>100:
+#     print("Marks should be 100 or less")
+# elif marks>90:
+#     print("O grade")
+# elif marks>80:
+#     print("A grade")
+# elif marks>65:
+#     print("B grade")
+# elif marks>35:
+#     print("C grade")
+# elif marks<35:
+#     print("Fail")
+# elif marks<0:
+#     print("Marks cannot be less than 0")
+# else:
+#     print("Invalid")
+
+# no = int(input("Enter a no :"))
+# if no<0:
+#     print("No. is negative")
+# elif no>0:
+#     print("No. is positive")
+# elif no==0:
+#     print("Print no. is 0")
+# else:
+#     print("Invalid input")
+
+# a=int(input("Enter first no. "))
+# b=int(input("Enter second no. "))
+# c=int(input("Enter third no. "))
+# if (a>=b) and (a>=c):
+#     l = a
+# elif (b>=a) and (b>=c):
+#     l = b
+# else:
+#     l = c
+# print("largest is", l)
+
